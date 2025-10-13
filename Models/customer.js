d2c1dd33335f8a85dc374b89aa8a1d5d8136fc35
dhawal <galadhawal512@gmail.com>
2025-10-13 12:27:37 +0530

    storing oders of customers

--- Models/customer.js
+++ Models/customer.js
@@ -1,3 +1,4 @@
+// user.js se relate mat kro
 const mongoose = require("mongoose");
 const { Schema } = mongoose; // const schema = mongoose.schema
 // schema = schema (shyad)
@@ -11,35 +12,42 @@ async function main() {
 }
 
 const orderSchema = new Schema({
+  // for orders .
   iteam: String,
   price: Number,
 });
 
 const customerSchema = new Schema({
+  // customer schema.
+  // for customer .
   name: String,
   orders: [
     {
-      type: Schema.Types.ObjectId, //didn't understand .
+      type: Schema.Types.ObjectId, //didn't understand this syntax (meaning -> order se order id yah store karna )
       ref: "Order",
     },
   ],
 });
 
-const Order = mongoose.model("Order", orderSchema);
-const Customer = mongoose.model("Customer", customerSchema);
+const Order = mongoose.model("Order", orderSchema); // order schema
+const Customer = mongoose.model("Customer", customerSchema); // customer schema.
 
 const addCustomer = async () => {
-  let cust1 = new Customer({
-    name: "Rahul Kumar",
-  });
+  // let cust1 = new Customer({
+  //   name: "Rahul Kumar",
+  // });
 
-  let order1 = await Order.findOne({ iteam: "chips" });
-  let order2 = await Order.findOne({ iteam: "chocolates" });
+  // let order1 = await Order.findOne({ iteam: "chips" });
+  // let order2 = await Order.findOne({ iteam: "chocolates" });
 
-  cust1.orders.push(order1); // iteam se pura object he dhund kr push kr rhe hai re cust1 meh
-  cust1.orders.push(order2);
+  // cust1.orders.push(order1); // iteam se pura object he dhund kr push kr rhe hai re cust1 meh
+  // cust1.orders.push(order2);
 
-  let result = await cust1.save();
+  // let result = await cust1.save();
+  // console.log(result); // terminal ke andr pura object
+  // // store hote dikhega but mongodb ke andr sirf id store hoga because humne schema aisa define kiya hai
+
+  let result = await Customer.find();
   console.log(result);
 };
 
